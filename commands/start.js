d7bf573cf039237a6fe7e58a08cc9ff72e57fd53
itays123 <itays2005@gmail.com>
2020-09-04 15:43:13 +0300

    coloring start script

--- commands/start.js
+++ commands/start.js
@@ -1,4 +1,5 @@
 const { spawn } = require('child_process');
+const { green } = require('../utils/colors');
 
 function setStartCommand(program) {
 	program
@@ -10,12 +11,13 @@ function setStartCommand(program) {
 
 			const child = spawn('npm', spawnArgs, { detached: true });
 
-			console.log('Initializing Greenpress..')
+			console.log(blue('Initializing Greenpress..'))
 
 			child.stdout.on('data', (data) => {
 				if(data && data.toString().includes('READY  Server listening')) {
-					console.log("Greenpress is running!\n\rTo stop it, use: greenpress stop");
-					console.log("\rTo populate it, use: greenpress populate");
+					console.log(green("Greenpress is running!"));
+					console.log(`"\n\rTo stop it, use: ${blue('greenpress stop')}"`)
+					console.log(`\rTo populate it, use: ${blue('greenpress populate')}`);
 					process.exit(0);
 				}
 			});
