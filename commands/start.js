46d120cacbac69d06ce91d149e1919a66c84a631
drorrose <rosenbach.dror@gmail.com>
2020-09-05 14:36:08 +0300

    added color scheme to start local feature

--- commands/start.js
+++ commands/start.js
@@ -1,4 +1,4 @@
-const { green, blue } = require('../utils/colors');
+const { green, blue, red } = require('../utils/colors');
 const { spawn } = require('child_process');
 
 const servicesEnvsAndRepos = {
@@ -18,29 +18,28 @@ function setStartCommand(program) {
 		.action(async function (mode = 'user', options) {
 
 			if (mode === 'dev' && options.local) {
-				console.log(`Chose to locally run ${options.local} services`)
+				console.log(blue(`Chose to locally run ${options.local} services`));
 				for (const service of options.local.split(',')) {
 					if (servicesEnvsAndRepos[service] !== undefined) {
 						process.env[servicesEnvsAndRepos[service][0]] = process.cwd() + '/dev/' + servicesEnvsAndRepos[service][1];
-						console.log(process.env[servicesEnvsAndRepos[service][0]])
+						console.log(green(process.env[servicesEnvsAndRepos[service][0]]));
 					} else {
-						console.log(`${service} is not a valid option, exiting!`)
+						console.log(red(`${service} is not a valid option, exiting!`));
 						process.exit(1);
 					}
 
 				}
 			}
 
-			const spawnArgs = mode === 'user' ? [ 'start' ] : [ 'run', 'dev' ]
-
+			const spawnArgs = mode === 'user' ? [ 'start' ] : [ 'run', 'dev' ];
 			const child = spawn('npm', spawnArgs, { detached: true });
 
-			console.log(blue('Initializing Greenpress..'))
+			console.log(blue('Initializing Greenpress..'));
 
 			child.stdout.on('data', (data) => {
 				if(data && data.toString().includes('READY  Server listening')) {
 					console.log(green("Greenpress is running!"));
-					console.log(`"\n\rTo stop it, use: ${blue('greenpress stop')}"`)
+					console.log(`"\n\rTo stop it, use: ${blue('greenpress stop')}"`);
 					console.log(`\rTo populate it, use: ${blue('greenpress populate')}`);
 					process.exit(0);
 				}
