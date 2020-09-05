05d7e74b6df967b224293b50b6a92087ffe78174
itays123 <itays2005@gmail.com>
2020-09-05 19:33:04 +0300

    bug fix

--- cli.js
+++ cli.js
@@ -1,20 +1,14 @@
 #!/usr/bin/env node
+const { version } = require("./package.json");
+const create = require("./commands/create");
 
-const program = require('commander')
-const { version } = require('./package.json')
-const create = require('./commands/create');
+const argv = require("yargs")
+  .usage("Usage: $0 <command> [options]")
+  .command(
+    "<create> [name] [type] [altFront] [mode]",
+    "create a new website using greenpress",
+  ).argv._;
 
-require('yargs')
-    .command(
-        'create [name] [type] [altFront] [mode]', 
-        'create a new website using greenpress',
-        ({ positional }) => {
-            positional('name', { default: 'greenpress' })
-            positional('type', { default: 'default' })
-            positional('altFront', { default: null })
-            positional('mode', { default: 'user' })
-        },
-        (argv) => {
-            create(argv.name, argv.type, argv.altFront, argv.mode);
-        }
-    )
+switch (argv._[0]) {
+    case 'create': create(argv);
+}
