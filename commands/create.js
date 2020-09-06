e443ef267b3da26a8d41d1b7c65959c502988ce9
itays123 <itays2005@gmail.com>
2020-09-06 10:59:56 +0300

    create command refactoring

--- commands/create.js
+++ commands/create.js
@@ -8,61 +8,56 @@ const execute = require('../services/execute');
 // 'create [name] [type] [altFront] [mode]'
 // 'create a new website using greenpress'
 async function create ({ name = 'greenpress', type = 'default', altFront = null, mode = 'user' }) {
-	// clone the greenpress base repo
-	const repoPath = type === 'pm2' ?
-		'https://github.com/greenpress/greenpress-pm2' :
-		'https://github.com/greenpress/greenpress';
-	const createCommand = `git clone ${repoPath} ${name}`;
 	try {
-		const stdout = execute(createCommand);
-		console.log(stdout);
-	} catch (err) {
-		console.log(red(err.message));
-		return;
-	}
+		// clone the greenpress base repo
+		const repoPath = type === 'pm2' ?
+			'https://github.com/greenpress/greenpress-pm2' :
+			'https://github.com/greenpress/greenpress';
+		const createOutput = execute(`git clone ${repoPath} ${name}`);
+		console.log(createOutput);
 
-	// check if user wants to change the alt front url-
-	if (altFront !== null) {
-		console.log(blue(`setting blog front to ${altFront}`));
-		await changeAltFront(name, altFront);
-	} else {
-		let changeAltFront = await accept(
-		  `Would you like to set alternative blog-front?`,
-		);
-		if (changeAltFront) {
-		    altFront = await askQuestion(
-		        `Select alternative blog-front: `,
-		        null
+		// check if user wants to change the alt front url-
+		if (altFront !== null) {
+			console.log(blue(`setting blog front to ${altFront}`));
+			await changeAltFront(name, altFront);
+		} else {
+			let changeAltFront = await accept(
+			`Would you like to set alternative blog-front?`,
 			);
-			if (altFront !== null) {
-				console.log(blue(`setting blog front to ${altFront}`));
-				await changeAltFront(name, altFront);
+			if (changeAltFront) {
+				altFront = await askQuestion(
+					`Select alternative blog-front: `,
+					null
+				);
+				if (altFront !== null) {
+					console.log(blue(`setting blog front to ${altFront}`));
+					await changeAltFront(name, altFront);
+				}
 			}
 		}
-	}
 
-	if (mode === 'user') {
-		try {
+		// remane remote repo if needed
+		if (mode === 'user') {
 			const output = execute(`cd ${name} && git remote rename origin gp`);
 			console.log(output);
-		} catch (err) {
-			console.log(err);
-			return;
 		}
-	}
 
-	console.log('\n', blue('Application is now installing..'), '\n')
+		// install all deps
+		console.log('\n', blue('Application is now installing..'), '\n')
+		try {	
+			execute(`cd ${name} && npm install`, { stdio: 'inherit' });
+		} catch (err) {
+			throw new Error('Failed to install Application');
+		}
 
-	try {	
-		execute(`cd ${name} && npm install`, { stdio: 'inherit' });
+		// done!
+		console.log(green('Done!'),
+			`\nEnter ${blue(name)} directory, You can run the application using: npm start`);
+		process.exit(0);
+
 	} catch (err) {
-		console.log(red('Failed to install Application'));
-		return;
+		console.log(red(err.message))
 	}
-
-	console.log(green('Done!'),
-		`\nEnter ${blue(name)} directory, You can run the application using: npm start`);
-	process.exit(0);
 }
 
 module.exports = create;
