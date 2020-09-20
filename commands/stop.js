54c22b5221d7a19abca138dccfe4f245d4c9bf13
drorrose <rosenbach.dror@gmail.com>
2020-09-20 19:58:28 +0300

    start: added debug option, separated code into services.

--- commands/stop.js
+++ commands/stop.js
@@ -1,48 +1,10 @@
-const { execSync } = require('child_process');
-const { green, blue } = require('../utils/colors');
+const { stopCommand } = require('../controllers/stop');
 
 function setStopCommand(program) {
 	program
 		.command('stop')
 		.description('stop greenpress application')
-		.action(function () {
-
-			console.log(blue('Stopping greenpress...'));
-			try {
-				execSync('npx pm2 stop db', () => {});
-			} catch (e) {
-				//
-			}
-			execSync(`npx pm2 stop all`, (error, stdout, stderr) => {
-				if (error) {
-					console.log(error.message);
-					return;
-				}
-
-				if (stderr) {
-					console.log(stderr);
-					return;
-				}
-
-				console.log(stdout);
-			});
-			execSync(`npx pm2 kill`, (error, stdout, stderr) => {
-				if (error) {
-					console.log(error.message);
-					return;
-				}
-
-				if (stderr) {
-					console.log(stderr);
-					return;
-				}
-
-				console.log(stdout);
-			});
-
-			console.log(green("Greenpress stopped"));
-			process.exit(0);
-		});
+		.action(stopCommand);
 }
 
 module.exports = setStopCommand;
