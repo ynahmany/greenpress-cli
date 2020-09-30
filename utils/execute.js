b8391746524852ea9af8b2b367b4ac65232d7a23
drorrose <rosenbach.dror@gmail.com>
2020-09-30 19:24:38 +0300

    refactored stop so it will use execute and will run all steps before it exits process. Wrapped execute's execSync with try-catch block to handle exceptions

--- utils/execute.js
+++ utils/execute.js
@@ -2,21 +2,27 @@ const { red } = require('./colors');
 const { execSync } = require('child_process');
 
 module.exports = function execute(cmd, actionDescription, execProps = null) {
-	execSync(cmd, execProps, (error, stdout, stderr) => {
-		if (error) {
-			console.log(red(actionDescription ? `Failed to ${actionDescription}: ${error.message}` : error.message));
+	try {
+		execSync(cmd, execProps, (error, stdout, stderr) => {
+			if (error) {
+				console.log(red(actionDescription ? `Failed to ${actionDescription}: ${error.message}` : error.message));
+				
+				return false;
+			}
 			
-			return false;
-		}
-		
-		if (stderr) {
-			console.log(red(actionDescription ? `Error occurred while trying to ${actionDescription}: ${error.message}` : stderr));
+			if (stderr) {
+				console.log(red(actionDescription ? `Error occurred while trying to ${actionDescription}: ${error.message}` : stderr));
+				
+				return false;
+			}
 			
-			return false;
-		}
-		
-		console.log(stdout);
-	});
+			console.log(stdout);
+		});
+	} catch (error) {
+		console.log(red(actionDescription ? `Error occurred while trying to ${actionDescription}: ${error.message}`: error.message));
+
+		return false;
+	}
 
 	return true;
 }
