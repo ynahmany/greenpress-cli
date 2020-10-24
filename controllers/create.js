e6ab3e0784c85fc528caa9d3cf751437424b1e7f
drorrose <rosenbach.dror@gmail.com>
2020-10-24 20:13:07 +0300

    corrected mongodb_volume's backslash to forward slash for windows

--- controllers/create.js
+++ controllers/create.js
@@ -2,7 +2,11 @@ const askQuestion = require('../utils/question');
 const accept = require('../utils/acceptance');
 const { clone, setServiceVersion, renameOrigin, installDependencies } = require('../services/create');
 const { red, green, blue } = require('../utils/colors');
+const { join } = require('path');
+const fs = require('fs');
 const execute = require('../utils/execute');
+const { execSync } = require('child_process');
+const { env } = require('process');
 const localCompositionGuide = 'https://docs.greenpress.info/guide/local-docker-composition.html'
 
 async function askAlternativeFront(defaultValue) {
@@ -28,6 +32,33 @@ module.exports = async function createController(name = 'greenpress', type = 'de
 
 	if (mode === 'user') {
 		renameOrigin(name);
+	}
+
+	if (process.platform === 'win32') {
+		try {
+			const composePath = join(process.cwd(), name, 'compose');
+			execSync('npm run envs', { cwd:  composePath });
+			
+			let envContent = '';
+			const envFilePath = join(composePath, '.env');
+			const envFile = fs.readFileSync(envFilePath).toString();
+			
+			const envParams = envFile.split('\n');
+			envParams.forEach(element => {
+				if (element.includes('MONGODB_VOLUME')) {
+					const volumeSpecs = element.split('=');
+					envContent += `${volumeSpecs[0]}=${volumeSpecs[1].replace('/', '\\')}\n`
+				} else {
+					envContent += `${element}\n`;
+				}
+			})
+	
+			fs.truncateSync(envFilePath, 0);
+			fs.writeFileSync(envFilePath, envContent);
+		} catch (e) {
+			console.log(red(`An unexpected error occured while setting env correctly. Error: ${e.message}`));
+			process.exit(1);	
+		}
 	}
 
 	console.log(green('Done!'),
