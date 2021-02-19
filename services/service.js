d42cf9ab82d1fbac4267116ebfd3e96e756623bb
drorrose <rosenbach.dror@gmail.com>
2021-02-19 21:08:07 +0200

    fix:changed scaled image names to correct ones

--- services/service.js
+++ services/service.js
@@ -5,33 +5,33 @@ const { join } = require('path');
 
 const devDir = join(process.cwd(), 'dev');
 const repos = {
-	'auth': 'https://github.com/greenpress/authentication-service',
 	'admin': 'https://github.com/greenpress/admin-panel',
-	'secrets': 'https://github.com/greenpress/secrets-service',
 	'assets': 'https://github.com/greenpress/assets-service',
+	'auth': 'https://github.com/greenpress/authentication-service',
 	'content': 'https://github.com/greenpress/content-service',
+	'drafts': 'https://github.com/greenpress/drafts-service',
 	'front': 'https://github.com/greenpress/blog-front',
-	'drafts': 'https://github.com/greenpress/drafts-service'
+	'secrets': 'https://github.com/greenpress/secrets-service'
 };
 
 const images = {
-	'auth': 'greenpress_greenpress_1',
 	'admin': 'greenpress_greenpress_1',
-	'secrets': 'greenpress_greenpress_1',
 	'assets': 'greenpress_greenpress_1',
+	'auth': 'greenpress_greenpress_1',
 	'content': 'greenpress_greenpress_1',
+	'drafts': 'greenpress_greenpress_1',
 	'front': 'greenpress_greenpress_1',
-	'drafts': 'greenpress_greenpress_1'
+	'secrets': 'greenpress_greenpress_1'
 };
 
 const scaledImages = {
-	'auth': 'greenpress_greenpress_1',
-	'admin': 'greenpress_greenpress_1',
-	'secrets': 'greenpress_greenpress_1',
-	'assets': 'greenpress_greenpress_1',
-	'content': 'greenpress_greenpress_1',
-	'front': 'greenpress_greenpress_1',
-	'drafts': 'greenpress_greenpress_1'
+	'admin': 'greenpress_admin_1',
+	'assets': 'greenpress_assets_1',
+	'auth': 'greenpress_auth_1',
+	'content': 'greenpress_content_1',
+	'drafts': 'greenpress_drafts_1',
+	'front': 'greenpress_front_1',
+	'secrets': 'greenpress_secrets_1'
 };
 
 function createDevDir() {
