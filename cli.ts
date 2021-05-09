c7a0ed6be9e8c12773563c84d40cc8e0593dcf60
yohainjug <yohainjug@gmail.com>
2021-05-09 09:44:38 +0300

    update cli

--- cli.ts
+++ cli.ts
@@ -2,7 +2,7 @@
 
 import { Command } from 'commander';
 const program = new Command();
-// const { version } = require('./package.json')
+const { version } = require('./package.json')
 import { setCreateCommand } from './commands/create';
 import { setUpgradeCommand } from './commands/upgrade';
 import { setPopulateCommand } from './commands/populate';
@@ -15,8 +15,8 @@ import { setDeploymentCommand } from './commands/deployment';
 import { setDeployCommand } from './commands/deploy';
 import { setLogsCommand } from './commands/logs';
 
-// program.version(version)
-program.version('0.6.2');
+program.version(version)
+// program.version('0.6.2')
 
 setCreateCommand(program);
 setUpgradeCommand(program);
@@ -29,15 +29,6 @@ setThemeCommand(program);
 setDeploymentCommand(program);
 setDeployCommand(program);
 setLogsCommand(program);
-// require('./commands/upgrade')(program)
-// require('./commands/populate')(program)
-// require('./commands/start')(program)
-// require('./commands/stop')(program)
-// require('./commands/missing')(program)
-// require('./commands/service')(program)
-// require('./commands/theme')(program)
-// require('./commands/deployment')(program)
-// require('./commands/deploy')(program)
-// require('./commands/logs')(program)
+
 
 program.parse(process.argv)
